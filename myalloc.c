01679ceba0cbad5eb13f298ad3d5cd64706919f4
Micah Stalberg <stalberm@oregonstate.edu>
2023-02-14 14:22:45 -0800

    Initial commit

---
+++ myalloc.c
@@ -0,0 +1,71 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/mman.h>
+
+#define ALIGNMENT 16   // Must be power of 2
+#define GET_PAD(x) ((ALIGNMENT - 1) - (((x) - 1) & (ALIGNMENT - 1)))
+
+#define PADDED_SIZE(x) ((x) + GET_PAD(x))
+#define PTR_OFFSET(p, offset) ((void*)((char *)(p) + (offset)))
+
+struct block {
+    struct block *next;
+    int size;
+    int in_use; 
+};
+
+struct block *head = NULL;
+
+void *myalloc(int size) {
+
+    if (head == NULL) {
+        head = mmap(NULL, 1024, PROT_READ|PROT_WRITE,
+                    MAP_ANON|MAP_PRIVATE, -1, 0);
+        head->next = NULL;
+        head->size = 1024 - PADDED_SIZE(sizeof(struct block));
+        head->in_use = 0;
+    }
+    struct block *curr_block = head;
+    while (curr_block != NULL && curr_block->in_use != 1) {
+        if (curr_block->size >= size) {
+            curr_block->in_use = 1;
+            int padded_block_size = PADDED_SIZE(sizeof(struct block));
+            return PTR_OFFSET(curr_block, padded_block_size);
+        }
+        curr_block = curr_block->next;
+    }
+    return NULL;
+}
+
+void print_data(void)
+{
+    struct block *b = head;
+
+    if (b == NULL) {
+        printf("[empty]\n");
+        return;
+    }
+
+    while (b != NULL) {
+        // Uncomment the following line if you want to see the pointer values
+        printf("[%p:%d,%s]\n", b, b->size, b->in_use? "used": "free");
+        printf("[%d,%s]", b->size, b->in_use? "used": "free");
+        if (b->next != NULL) {
+            printf(" -> ");
+        }
+
+        b = b->next;
+    }
+
+    printf("\n");
+}
+
+int main(int argc, char *argv[]) {
+    void *p;
+
+    print_data();
+    p = myalloc(16);
+    print_data();
+    p = myalloc(16);
+    printf("%p\n", p);
+}
